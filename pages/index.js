1615c50bdcc53306b42f1cc9b6ab37d673bc7311
Quentin Fonck <quentin.fonck@stringit.lu>
2021-12-04 15:27:44 +0100

    Removed back button link on first-post + changed description message on main page

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }) {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>It's me, Mario!</p>
+                <p>A basic blog I made to use nextjs. Nothing to see here!</p>
                 <p>
                     (This is a sample website - you’ll be building a site like
                     this on{" "}
